2d22814ef0bf32dbc895376f7e8e70a940b806b5
Mason Keiser <masonksr5@gmail.com>
2020-09-15 00:27:39 +0000

    backend post request to user info table

--- server/index.js
+++ server/index.js
@@ -29,6 +29,23 @@ app.get('/api/user_info', (req, res, next) => {
       .catch(err => next(err));
 });
 
+app.post('/api/sign_up', (req, res, next) => {
+  if (!req.body) {
+    return next(new ClientError('Missing required content', 404))
+  }
+  const sql = `
+  INSERT INTO "user_info" ("user_name", "user_email", "user_password")
+  VALUES                  ($1, $2, $3)
+  RETURNING *;
+  `
+  const params = [req.body.user_name, req.body.user_email, req.body.user_password];
+  db.query(sql, params)
+    .then(result => {
+      const row = result.rows[0]
+      res.status(201).json(row);
+    })
+})
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });

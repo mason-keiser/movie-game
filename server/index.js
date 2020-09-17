acc00c8119620904246e0161497de312ba2c11d9
Mason Keiser <masonksr5@gmail.com>
2020-09-17 01:19:37 +0000

    can search database ffor login info

--- server/index.js
+++ server/index.js
@@ -19,6 +19,8 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
+//  SEARCH THROUGH USER_INFO TABLE (RETURNS ALL VALUES)
+
 app.get('/api/user_info', (req, res, next) => {
     const sql = `
     SELECT *
@@ -28,6 +30,8 @@ app.get('/api/user_info', (req, res, next) => {
       .then(result => res.json(result.rows))
       .catch(err => next(err));
 });
+
+//  SIGN UP API POST REQUEST THAT ADDS USER INFO TO DB 
 
 app.post('/api/signUp', (req, res, next) => {
   if (!req.body.user_name) {
@@ -50,6 +54,31 @@ app.post('/api/signUp', (req, res, next) => {
       const row = result.rows[0]
       res.status(201).json(row);
     })
+})
+
+//  SEARCH DATABASE FOR EXISTING USER_EMAIL AND USER_PASSWORD API GET REQUEST
+
+app.get('/api/login', (req, res, next) => {
+  const email = req.body.user_email;
+  const password = req.body.user_password;
+  const sql = `
+  SELECT * FROM "user_info"
+  WHERE "user_email" = $1 
+  AND "user_password" = $2;
+  `
+  const params = [req.body.user_email, req.body.user_password]
+  db.query(sql, params)
+    .then(result => {
+      if (!result.rows[0]) {
+        return res.status(200).json({ message: `No user information contain: ${email} or ${password}` });
+      } else {
+        return res.status(200).json(result.rows);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    });
 })
 
 app.use('/api', (req, res, next) => {

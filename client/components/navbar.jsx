539bdde72d4208c3a76db5c1001927893eff7962
Mason Keiser <masonksr5@gmail.com>
2020-09-23 17:00:32 +0000

    matches logo added to nav

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -94,7 +94,7 @@ export default class Header extends React.Component {
           </Navbar>
         </Container>
         );
-      } else {
+      } else if (this.props.view.name === 'main') {
         return (
           <Container fluid={true} className="py-3 bg-white sticky-top shadow-sm">
             <Navbar color="faded" light
@@ -145,6 +145,59 @@ export default class Header extends React.Component {
             </NavbarBrand>
           </Navbar>
         </Container>
+        )
+      } else if (this.props.view.name === 'game' || this.props.view.name === 'matches_page') {
+        return (
+          <Container fluid={true} className="py-3 bg-white sticky-top shadow-sm">
+          <Navbar color="faded" light
+            expand="md"
+            className="row py-0 ">
+            <NavbarToggler onClick={this.handleToggle} navbar="true" style={{border: 'none'}}/>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="" navbar>
+            <div>
+                <a
+                  offset={offset}
+                  duration={1000}
+                  className="pointer px-0 nav-link mt-2"
+                  onClick={this.handleToggle, () => {this.props.setView('home', {})}}>
+                   Start Game
+                </a>
+              </div>
+              <div className="ml-md-5">
+                <a
+                  href="https://masonkeiser.com/"
+                  offset={offset}
+                  duration={1000}
+                  className="pointer px-0 nav-link mt-2"
+                  onClick={this.handleToggle}>
+                  Mason's Portfolio
+                </a>
+              </div>
+              <div className="ml-md-5">
+                <a
+                  href="https://andrewsong.org/"
+                  offset={offset}
+                  duration={1000}
+                  className="pointer px-0 nav-link mt-2"
+                  onClick={this.handleToggle}>
+                  Andrew's Portfolio
+                </a>
+              </div>
+            </Nav>
+          </Collapse>
+          <NavbarBrand className="pointer decoration-none" onClick={() => {
+              scroll.scrollToTop();
+              if (this.state.isOpen) {
+                this.handleToggle();
+              }
+              this.props.setView('home', {})
+          }}>
+            Movie Match
+          </NavbarBrand>
+          <div className='mr-4 mt-1 fa fa-envelope' style={{ position: 'absolute', right: '0',}}></div>
+        </Navbar>
+      </Container>
         )
       }
     }

7fdb2fb549a63cad24c17b964ee6eb6e1ae45a9d
Andrew <awhsg11@gmail.com>
2020-09-24 03:57:42 +0000

    Created MovieGame Component

---
+++ client/components/game_prep.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class GamePrep extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (null);
+  }
+}
